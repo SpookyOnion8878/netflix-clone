7018aadc91513ea101cb2a93bcc47f510be25b8f
Spooky Onion <spookyonion8878@gmail.com>
2023-09-06 02:14:48 +0700

    add new file : AccountMenu, Modified MobileMenu, Navbar, Note : use Bactick ` if you coding rotate-180 icon menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,21 @@
 import { useCallback, useState } from 'react';
-import {BsChevronDown} from 'react-icons/bs';
+import {BsChevronDown, BsSearch, BsBell} from 'react-icons/bs';
 
 import NavbarItem from "./NavbarItem";
 import MobileMenu from "./MobileMenu";
+import AccountMenu from './AccountMenu';
 
 
 const Navbar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current)
+    }, []);
+    
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current)
     }, []);
  
     return (
@@ -57,11 +63,25 @@ const Navbar = () => {
                     "
                 >
                     <p className="text-white text-m">Browse</p>
-                    <BsChevronDown className="text-white transition" />
+                    <BsChevronDown className={`w-4 text-white fill-white ${showMobileMenu ? 'rotate-180' : 'rotate-0' } `} />
                     <MobileMenu visible= {showMobileMenu} />
                 </div>
-                <div>
-                    
+                <div className='flex flex-row ml-auto gap-7 items-center'>
+                    <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                        <BsSearch />
+                    </div>
+                    <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                        <BsBell />
+                    </div>
+
+                    <div onClick={toggleAccountMenu} className='flex flex-row items-center gap-2 cursor-pointer relative'>
+                        <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
+                        <img src='/images/default-blue.png' alt='' /> 
+                        </div>
+                        
+                        <BsChevronDown className={`w-4 text-white fill-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+                        <AccountMenu visible={showAccountMenu} />
+                    </div>
                 </div>
             </div>
         </nav>
